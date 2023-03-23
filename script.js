0f9e22c813a3f31d6ff4db52cc6e85d97a1b081a
mehran vejdani <mehran.vejdanii@gmail.com>
2023-03-23 15:00:20 +0430

    getSizeFromImg

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const uploadBox = document.querySelector(".upload-box");
 const fileInput = uploadBox.querySelector("input");
 const preViewImg = uploadBox.querySelector("img");
+const widthInput = document.querySelector(".width input");
+const heightInput = document.querySelector(".height input");
 
 const loadFile = (e) => {
   const file = e.target.files[0]; //getting first user selected file
   if (!file) return;
   preViewImg.src = URL.createObjectURL(file);
   preViewImg.addEventListener("load", () => {
+    widthInput.value = preViewImg.naturalWidth;
+    heightInput.value = preViewImg.naturalHeight;
+
     document.querySelector(".wrapper").classList.add("active");
   });
 };
